bc1853c07478a2ef30b221549e428afbb15ad7ca
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-01-22 10:17:33 +0900

    pro2: create basic webserver using socket.h

--- project2/src/main.c
+++ project2/src/main.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int main()
+int main(void)
 {
-  printf("hello world!");
+  int rsock, wsock;
+  struct sockaddr_in addr, client;
+  int len;
+  int ret;
+
+  rsock = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (rsock < 0)
+  {
+    fprintf(stderr, "Error. Cannot make socket\n");
+    return -1;
+  }
+
+  // socket setting
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(3000);
+  addr.sin_addr.s_addr = INADDR_ANY;
+
+  // bind socket
+  ret = bind(rsock, (struct sockaddr *)&addr, sizeof(addr));
+
+  if (ret < 0)
+  {
+    fprintf(stderr, "Error. Cannot bind socket\n");
+  }
+
+  // listen to socket
+  listen(rsock, 5);
+
+  // accept tcp connections
+  len = sizeof(client);
+  wsock = accept(rsock, (struct sockaddr *)&client, &len);
+
+  // send message
+  write(wsock, "HTTP1.1 200 OK", 14);
+
+  // close tcp connection
+  close(wsock);
+  close(rsock);
+
   return 0;
 }
